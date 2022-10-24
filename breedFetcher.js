62f0fc47495e4a39a2e71db73b713c162fa16dad
Lucas Sahota <lucaswade4@gmail.com>
2022-10-23 20:09:38 -0700

    refactoring to use mocha and chai to test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,17 @@
 const request = require('request');
-let search = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${search}`, (error, response, body) => {
-  if (error) {
-    console.log(`ERROR: ${error.code}`);
-    return;
-  }
-  if (body.length < 3) {
-    console.log(`The ${search} breed not found!`);
-    return;
-  }
-  const data = JSON.parse(body);
-  console.log(data[0].description);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(`${error}`, null);
+    }
+    const data = JSON.parse(body);
+    if (data[0]) {
+      callback(null, data[0].description);
+    } else {
+      callback(`Error: the ${breedName} breed was not found`, null);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
---
+++ index.js
@@ -0,0 +1,11 @@
+const { fetchBreedDescription } = require('./breedFetcher');
+
+let breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log(error);
+  } else  {
+    console.log(desc);
+  }
+});
